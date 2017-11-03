07c410ecd5460d8174154a97e745098169875478
Orest DEV-10 <orest@proficiencyconsulting.com>
2017-11-03 08:13:10 -0400

    MVC is integrated with Angular

---
+++ MvcAndAngular/Controllers/AppController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAndAngular.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Shop() {
+            return View();
+        }
+    }
+}
